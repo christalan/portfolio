3c3edb4981ade20b7d70058eaf800cd1ab43056b
alan-christian <alancsystem@gmail.com>
2023-05-03 13:20:09 -0300

    Update projetos.component.ts

--- src/app/projetos/projetos.component.ts
+++ src/app/projetos/projetos.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 export class ProjetosComponent {
   projects = [
     { "img": "../../assets/pokedex.png", "link": "https://pokedex-theta-sage.vercel.app/" },
-    { "img": "../../assets/coffee.png", "link": "https://rocket-coffee-chi.vercel.app/" },
+    { "img": "../../assets/coffee.png", "link": "https://rocket-coffee-beta.vercel.app/" },
     { "img": "../../assets/calculadora.png", "link": "https://calculadora-three-bay.vercel.app/" },
     { "img": "../../assets/consulta cep.png", "link": "https://consulta-de-cep-eight.vercel.app/" },
     { "img": "../../assets/em breve.png", "link": "#" },
