165efeccacba0b9d0488d5f2cd158fa65fcf4f3c
alan <alancsystem@gmail.com>
2023-05-07 14:41:56 -0300

    atualiza links

--- src/app/projetos/projetos.component.ts
+++ src/app/projetos/projetos.component.ts
@@ -6,11 +6,11 @@ import { Component } from '@angular/core';
 })
 export class ProjetosComponent {
   projects = [
-    { "img": "../../assets/pokedex.png", "link": "https://pokedex-theta-sage.vercel.app/" },
-    { "img": "../../assets/coffee.png", "link": "https://rocket-coffee-beta.vercel.app/" },
-    { "img": "../../assets/calculadora.png", "link": "https://calculadora-three-bay.vercel.app/" },
-    { "img": "../../assets/consulta cep.png", "link": "https://consulta-de-cep-eight.vercel.app/" },
-    { "img": "../../assets/em breve.png", "link": "#" },
+    { "img": "../../assets/pokedex.png", "link": "https://pokedex-ma9r.onrender.com/" },
+    { "img": "../../assets/coffee.png", "link": "https://rocket-coffee.onrender.com/" },
+    { "img": "../../assets/be the hero.png", "link": "https://be-the-hero-51bh.onrender.com/" },
+    { "img": "../../assets/consulta cep.png", "link": "https://consulta-de-cep.onrender.com/" },
+    { "img": "../../assets/calculadora.png", "link": "https://calculadora-t4wb.onrender.com/" },
     { "img": "../../assets/em breve.png", "link": "#" }
   ]
 }
